09a92f2e580343db517a88a6bd56dea00b25bdb1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-11 06:20:18 +0000

    Deploy bmac.vecha.in to github.com/libotony/buy-me-a-coffee.git:gh-pages

---
+++ precache-manifest.8313cdf8643f4f22e7047ede3318afd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1072360383c25740c2bd",
+    "url": "/css/app.76adb526.css"
+  },
+  {
+    "revision": "1072360383c25740c2bd",
+    "url": "/js/app.3ae97d9a.js"
+  },
+  {
+    "revision": "6feb1ecca7c9b6c1e682",
+    "url": "/js/chunk-vendors.0671c24d.js"
+  },
+  {
+    "revision": "115432152a0ed073e6303654dcea05c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
